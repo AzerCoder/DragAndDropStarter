53c383c6cf6d18319d23646f874a3116dbb13c5f
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-09-27 16:10:05 +0500

    Initial Commit

---
+++ DragAndDropStarter/DragAndDropStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropStarterApp.swift
+//  DragAndDropStarter
+//
+//  Created by A'zamjon Abdumuxtorov on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
